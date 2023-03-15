46b858809a274388c7e6cecb31eba7db82d18758
anandu <sonysandr@gmail.com>
2023-03-16 01:55:51 +0530

    todoapp first commit add to list and delete from list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleNewTask = (e) => {
+    setNewTask(e.target.value);
+    console.log(newTask);
+  };
+  const handleAddTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const handleDeleteTask = (id) => {
+    // const newTodoList = todoList.filter((task)=>task !== taskName)
+    // setTodoList(newTodoList)
+    // instead we can do it in one line
+
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ backgroundColor: "darkgray" }}>
+        <input
+          onChange={handleNewTask}
+          type="text"
+          placeholder="Add tasks here"
+        />
+        <span>&nbsp;</span>
+        <button style={{ backgroundColor: "green" }} onClick={handleAddTask}>
+          Add List
+        </button>
+      </div>
+      <div style={{ backgroundColor: "lightgray" }}>
+        {todoList.map((task) => {
+          return (
+            <div
+              style={{ display: "flex", flexFlow: "row", alignItems: "center" }}
+            >
+              <h1>{task.taskName}</h1>
+              <button
+                style={{ backgroundColor: "red" }}
+                onClick={() => handleDeleteTask(task.id)}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
